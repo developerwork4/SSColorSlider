cd83ba9a57e1eea51deecb9133b906d3d33cdf41
sweta sheth <developer.work412@gmail.com>
2020-04-04 16:00:26 +0530

    initial setup.

---
+++ SSColorSlider/SSColorSlider.h
@@ -0,0 +1,19 @@
+//
+//  SSColorSlider.h
+//  SSColorSlider
+//
+//  Created by Sweta on 04/04/20.
+//  Copyright © 2020 Sweta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SSColorSlider.
+FOUNDATION_EXPORT double SSColorSliderVersionNumber;
+
+//! Project version string for SSColorSlider.
+FOUNDATION_EXPORT const unsigned char SSColorSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSColorSlider/PublicHeader.h>
+
+
